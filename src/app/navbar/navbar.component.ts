814086976f2fe968564b0aa24773605c189a1015
Bryan <72945681+bryancodesjs@users.noreply.github.com>
2021-10-11 18:32:57 -0400

    navbar toggle corregido

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,5 +11,13 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  lightNav() {
+    if ( (document.getElementById('navigationBar') as HTMLElement).className == "navbar navbar-expand-lg responsive--background" ) {
+      (document.getElementById('navigationBar') as HTMLElement).className = "navbar navbar-expand-lg home--nav";
+    }
+    else {
+      (document.getElementById('navigationBar') as HTMLElement).className = "navbar navbar-expand-lg responsive--background";
+    }
+    
+  }
 }
