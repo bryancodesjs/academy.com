a1450a67fb68c85017bea1bc40a51967f45c8553
bryancodesjs <72945681+bryancodesjs@users.noreply.github.com>
2021-10-13 15:56:25 -0400

    courses are looped over a json file

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,5 +11,45 @@ export class ProfileComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  title = 'objectToArray';
+  courses = {
+    course: {
+      title:"Blockchain 101",
+      author:"John Doe", 
+      link:"https://images.pexels.com/photos/5980856/pexels-photo-5980856.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      students:"456"
+    },
+    course1: {
+      title:"Introduction to day trading",
+      author:"William Kirks", 
+      link:"https://images.pexels.com/photos/6802042/pexels-photo-6802042.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      students:"1,598"
+    },
+    course2: {
+      title:"GPU Mining from zero to profit",
+      author:"Sanada Otsuki",
+      link:"https://images.pexels.com/photos/4581902/pexels-photo-4581902.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      students:"993"
+    },
+    course3: {
+      title:"Create (and sell) your own NFTs without blockchain nor drawing skills",
+      author:"Sanada Otsuki",
+      link:"https://imgsrv2.voi.id/4qPftfPI6oHrJKXRzhgBExPwlUzArjSZTa6C1ezr1aU/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy81MzE2MS8yMDIxMDUyMTE1MDctbWFpbi5jcm9wcGVkXzE2MjE1ODk5MDYuanBn.jpg",
+      students:"8,470"
+    },
+    course4: {
+      title:"Blockchain: Foundations and Use Cases",
+      author:"ConsenSys",
+      link:"https://s3.amazonaws.com/media.al-fanarmedia.org/wp-content/uploads/2021/01/26160334/source-Hoyes1.jpg",
+      students:"2,046"
+    },
+    course5: {
+      title:"Ethereum and Solidity",
+      author:"Alexeyv Nklosa",
+      link:"https://mk0criptonoticijjgfa.kinstacdn.com/wp-content/uploads/2021/02/solidity-lenguaje-programacion-actualizacion.jpg",
+      students:"2,046"
+    }
+  };
 
+  data = Object.values(this.courses);
 }
