5065dd615e9d980af5b936e6cf748f3cc25fb6f7
Bryan <72945681+bryancodesjs@users.noreply.github.com>
2021-10-15 20:59:53 -0400

    course view improvements

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-course',
@@ -6,7 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
-
+  @HostListener("window:scroll", ['$event'])
+  scrollRead(){
+    let offset = scrollY;
+    let controls = (document.getElementById('sideMenu') as HTMLElement);
+    let main = (document.getElementById('main') as HTMLElement);
+    if (controls.className == "d-none") {
+      controls.className = "d-none";
+    } else {
+      if (scrollY > 80) {
+        controls.className = "fixed--to--screen col-lg-3 d-flex flex-column";
+      }
+      else {
+        controls.className = "col-lg-3 d-flex flex-column";
+      }
+    }
+  }
+  toggleSideMenu(){
+    let controls = (document.getElementById('sideMenu') as HTMLElement);
+    let main = (document.getElementById('main') as HTMLElement);
+    let toggleClip = (document.getElementById('toggleOnClip') as HTMLElement);
+    if (controls.className == "fixed--to--screen col-lg-3 d-flex flex-column") {
+      controls.className = "d-none";
+      main.className = "d-flex flex-column";
+      toggleClip.className = "toggle--on--clip show"
+    } else {
+      if (controls.className == "col-lg-3 d-flex flex-column") {
+        controls.className = "d-none";
+        main.className = "d-flex flex-column";
+        toggleClip.className = "toggle--on--clip show";
+      } else {
+        controls.className = "col-lg-3 d-flex flex-column";
+        main.className = "col-lg-9"
+        toggleClip.className = "toggle--on--clip";
+      }
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
